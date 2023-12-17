f0a0dbb7174a03c1898922b2e1c95f49215522fd
Ashok Choudhary <ashoka8769@gmail.com>
2023-12-17 18:18:16 +0530

    endpoints

--- app.js
+++ app.js
@@ -1,9 +1,48 @@
+const fs = require("fs");
+
 const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("hello man!!");
+app.use(express.json());
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, "utf-8"));
+
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    results: tours.length,
+    data: { tours },
+  });
+});
+
+app.get("/api/v1/tours/:id", (req, res) => {
+  const tour = tours.find((el) => el.id === +req.params.id);
+  // if (+req.params.id > tours.length)
+  if (!tour)
+    return res.status(404).json({
+      status: "fail",
+      message: "Invalid Id",
+    });
+  res.status(200).json({
+    status: "success",
+    data: { tour },
+  });
+});
+
+app.post("/api/v1/tours", (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+    res.status(201).json({
+      status: "success",
+      data: { tour: newTour },
+    });
+    if (err) res.status(500).send("faild");
+  });
 });
 
 const port = 3000;
