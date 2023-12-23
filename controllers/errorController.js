088e4508c56dee4fddd1576d5d8ba36d765fe095
Ashok Choudhary <ashoka8769@gmail.com>
2023-12-23 13:15:15 +0530

    handling express errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -29,6 +29,18 @@ function handleCastErrorDB(err) {
   const message = `Invalid ${err.path} ${err.value}`;
   return new AppError(message, 400);
 }
+function handleDuplicateFieldsDb(err) {
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/).at(0);
+  const message = `Duplicate field value: ${value}, Please use another value!`;
+  return new AppError(message, 400);
+}
+function handlesValidationErrorDb(err) {
+  const errorsMessage = Object.values(err.errors)
+    .map((value, i) => `${i + 1}. ${value.message}`)
+    .join("/n");
+  const message = `Invalid input data: ${errorsMessage}`;
+  return new AppError(message, 400);
+}
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
@@ -38,9 +50,9 @@ module.exports = (err, req, res, next) => {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === "production") {
     let error = { ...err };
-    if (err.name === "CastError") {
-      error = handleCastErrorDB(error);
-    }
+    if (err.name === "CastError") error = handleCastErrorDB(error);
+    if (err.code === 11000) error = handleDuplicateFieldsDb(err);
+    if (err.name === "ValidationError") error = handlesValidationErrorDb(err);
 
     sendErrorPro(error, res);
   }
