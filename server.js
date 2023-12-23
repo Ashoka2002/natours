a7d34e9650d9dd0477ba07e11edc2c1a72dadddb
Ashok Choudhary <ashoka8769@gmail.com>
2023-12-23 13:24:44 +0530

    Handling Unhandled Rejections

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ mongoose
   .then(() => console.log("DB connected succesfully!!"));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App is running on port ${port}`);
+});
+
+process.on("unhandledRejection", err => {
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
