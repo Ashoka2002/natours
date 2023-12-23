63f99a0cf4b1255887801b7fb9023e32bd8c5667
Ashok Choudhary <ashoka8769@gmail.com>
2023-12-23 16:08:17 +0530

    encrypting user password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,21 @@
 const express = require("express");
 
 const { getAllusers, getUser, createuser, updateUser, deleteUser } = require("../controllers/userController");
+const { signup } = require("../controllers/authController");
+
 const router = express.Router();
 
-router.route("/").get(getAllusers).post(createuser);
-router.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
+router.post("/signup", signup);
+
+router
+  .route("/")
+  .get(getAllusers)
+  .post(createuser);
+
+router
+  .route("/:id")
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
 module.exports = router;
