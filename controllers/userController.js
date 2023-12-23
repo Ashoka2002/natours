37d96c9361709cbb2b647ab4fa47f69331379b63
Ashok Choudhary <ashoka8769@gmail.com>
2023-12-23 16:08:17 +0530

    encrypting user password

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,30 +1,31 @@
 exports.getAllusers = (req, res) => {
   res.status(500).json({
     status: "error",
-    message: "This route not defined yet!!",
+    message: "This route not defined yet!!"
   });
 };
 
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: "error",
+    message: "this route not defined yet"
   });
 };
 
 exports.createuser = (req, res) => {
   res.status(500).json({
-    status: "error",
+    status: "error"
   });
 };
 
 exports.updateUser = (req, res) => {
   res.status(500).json({
-    status: "error",
+    status: "error"
   });
 };
 
 exports.deleteUser = (req, res) => {
   res.status(500).json({
-    status: "error",
+    status: "error"
   });
 };
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: [true, "Please tell us you name"] },
@@ -17,7 +18,25 @@ const userSchema = new mongoose.Schema({
     minlength: [8, "Password must have 8 character"]
   },
 
-  passwordConfirm: { type: String, required: [true, "Please confirm your password"] }
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please confirm your password"],
+    validate: {
+      //this only works on creating or saveing
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: "Passwords are not same!"
+    }
+  }
+});
+
+userSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  console.log(this.password);
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
