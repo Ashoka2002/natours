bceddcacdc8cfc1bd7c99860f99a02fd2cd522e8
Ashok Choudhary <ashoka8769@gmail.com>
2023-12-23 08:18:26 +0530

    creting error class

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
+const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
 const userRouter = require("./routes/userRoutes");
+const AppError = require("./utils/appErrors");
 
 const app = express();
 
@@ -27,20 +29,13 @@ app.all("*", (req, res, next) => {
   //   status: "fail",
   //   message: `Can't find ${req.originalUrl} on this server!!`
   // });
-  const err = new Error(`Can't find ${req.originalUrl} on this server!!`);
-  err.statusCode = 404;
-  err.status = "fail";
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!!`);
+  // err.statusCode = 404;
+  // err.status = "fail";
 
-  next(err);
+  next(new AppError(`Can't find ${req.originalUrl} on this server!!`, 404));
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
-});
+app.use(globalErrorHandler);
 // SERVER
 module.exports = app;
