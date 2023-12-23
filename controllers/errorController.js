c8e6408817aeeeba86d1b1f51c603182fdb702e9
Ashok Choudhary <ashoka8769@gmail.com>
2023-12-23 10:16:00 +0530

    adding isOperational 1

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,47 @@
+const AppError = require("../utils/appErrors");
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorPro = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  } else {
+    console.error("ERROR!!! 😱", err);
+
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong!"
+    });
+  }
+};
+
+function handleCastErrorDB(err) {
+  const message = `Invalid ${err.path} ${err.value}`;
+  return new AppError(message, 400);
+}
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === "production") {
+    let error = { ...err };
+    if (err.name === "CastError") {
+      error = handleCastErrorDB(error);
+    }
+
+    sendErrorPro(error, res);
+  }
 };
