c9ecec697791318adcb3f87be1343f4f22bce8d3
Ashok Choudhary <ashoka8769@gmail.com>
2023-12-25 13:43:47 +0530

    reset password part 1

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 
 const { getAllusers, getUser, createuser, updateUser, deleteUser } = require("../controllers/userController");
-const { signup, login } = require("../controllers/authController");
+const { signup, login, forgotPassword, resetPassword } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.post("/forgotPassword", forgotPassword);
+router.post("/resetPassword", resetPassword);
 
 router
   .route("/")
