2e18b70bde7f7456250563d2c55e0a605913d69f
Ashok Choudhary <ashoka8769@gmail.com>
2023-12-25 14:58:15 +0530

    sending emails

---
+++ utils/email.js
@@ -0,0 +1,27 @@
+const nodemailer = require("nodemailer");
+
+async function sendEmail(options) {
+  //CRETING TRANSPORTER
+  const transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD
+    }
+  });
+
+  //DEFINE THE EMAIL OPTION
+  const mailOption = {
+    from: "Ashok Choudhary <ashok@jonas.io>",
+    to: options.email,
+    subject: options.subject,
+    text: options.message
+  };
+
+  //SEND THE EMAIL
+
+  await transport.sendMail(mailOption);
+}
+
+module.exports = sendEmail;
