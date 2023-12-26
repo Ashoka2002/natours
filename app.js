926192f3a111247a2e5683c41d07e56711097da7
Ashok Choudhary <ashoka8769@gmail.com>
2023-12-26 15:47:14 +0530

    prevent parameter polution

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const globalErrorHandler = require("./controllers/errorController");
 const tourRouter = require("./routes/tourRoutes");
@@ -29,6 +32,19 @@ app.use("/api", limiter);
 
 //BODY PARSER, READING DATA FROM BODY INTO req.body
 app.use(express.json({ limit: "20kb" }));
+
+//DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
+app.use(mongoSanitize());
+
+//DATA SANITIZATION AGAINST XSS
+app.use(xss());
+
+//PREVENT PARAMETER POLUTION
+app.use(
+  hpp({
+    whitelist: ["duration", "ratingsAverage", "ratingsQuantity", "maxGroupSize", "difficulty"]
+  })
+);
 
 //SERVING STATIC FILES
 app.use(express.static("./public"));
