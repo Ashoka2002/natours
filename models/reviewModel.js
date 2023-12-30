ab3a4fffc0e855fab719b9b01d602af8cd8c4c10
Ashok Choudhary <ashoka8769@gmail.com>
2023-12-30 13:14:46 +0530

    fixing

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -5,8 +5,8 @@ const reviewSchema = new mongoose.Schema(
     review: { type: String, required: [true, "Review can not be empty"], trim: true, maxLength: 150 },
     rating: { type: Number, max: 5, min: 1, default: 1 },
     createdAt: { type: Date, default: Date.now() },
-    tour: [{ type: mongoose.Schema.ObjectId, ref: "Tour", required: [true, "Review must belong to a tour"] }],
-    user: [{ type: mongoose.Schema.ObjectId, ref: "User", required: [true, "Review must belong to a author"] }]
+    tour: { type: mongoose.Schema.ObjectId, ref: "Tour", required: [true, "Review must belong to a tour"] },
+    user: { type: mongoose.Schema.ObjectId, ref: "User", required: [true, "Review must belong to a author"] }
   },
   {
     toJSON: { virtuals: true },
