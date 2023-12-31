53345bdb9b063013ee43f5d84b1528204f312e2f
Ashok Choudhary <ashoka8769@gmail.com>
2023-12-31 09:24:57 +0530

    protecting routes part 1

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -26,6 +26,7 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/forgotPassword", forgotPassword);
 router.patch("/resetPassword/:token", resetPassword);
+
 router.patch("/updatePassword", protect, updatePassword);
 router.patch("/updateMe", protect, updateMe);
 router.delete("/deleteMe", protect, deleteMe);
