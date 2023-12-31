5f7e45f47b4ad297947800828795bd505d956f83
Ashok Choudhary <ashoka8769@gmail.com>
2023-12-31 09:16:24 +0530

    adding /me endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -49,6 +49,11 @@ exports.createUser = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
 exports.getAllusers = getAll(User);
 exports.getUser = getOne(User);
 // DO NOT ATTEMPT TO CHANGE PASSWORD USIGN THIS updateUser HANDLER
