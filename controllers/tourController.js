acf3d3a8e55af109911167c7839c220a627f88d6
Ashok Choudhary <ashoka8769@gmail.com>
2024-01-04 15:33:32 +0530

    Calculating Distances

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -92,6 +92,40 @@ exports.getTourWithin = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.getDistances = catchAsync(async (req, res, next) => {
+  const { latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(",");
+  if (!lat || !lng) next(new AppError("Please provide latitude and longitude in lat,lng format", 400));
+
+  const multiplier = unit === "mi" ? 0.000621371 : 0.001;
+
+  const distances = await Tour.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: "Point",
+          coordinates: [lng * 1, lat * 1]
+        },
+        distanceField: "distance",
+        distanceMultiplier: multiplier
+      }
+    },
+    {
+      $project: {
+        name: 1,
+        distance: 1
+      }
+    }
+  ]);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      data: distances
+    }
+  });
+});
+
 // exports.deleteTour = catchAsync(async (req, res, next) => {
 //   const tour = await Tour.findByIdAndDelete(req.params.id);
 
