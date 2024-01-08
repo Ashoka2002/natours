c2f549518dbeacb712a3a3dae395bbbaec3a0eb8
Ashok Choudhary <ashoka8769@gmail.com>
2024-01-08 12:50:15 +0530

    Building the Tour Overview - Part 1

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,16 @@
-exports.getOverview = (req, res) => {
+const Tour = require("../models/tourModel");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getOverview = catchAsync(async (req, res) => {
+  // 1) get tours
+  const tours = await Tour.find();
+
+  // 2) render template with tours data
   res.status(200).render("overview", {
-    title: "All tours"
+    title: "All tours",
+    tours
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render("tour", {
