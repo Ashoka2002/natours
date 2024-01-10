db29a813de3d669bcfe3c62984c0c441592d1d44
Ashok Choudhary <ashoka8769@gmail.com>
2024-01-10 13:42:31 +0530

    completing map

--- public/js/leaflet.js
+++ public/js/leaflet.js
@@ -1,24 +1,23 @@
 /* eslint-disable */
 const locations = JSON.parse(document.getElementById("map").dataset.locations);
 
-// const map = L.map("map", { zoomControl: false }); //to disable + - zoom
-var map = L.map("map", { zoomControl: false }).setView([31.111745, -118.113491], 6);
+var map = L.map("map", { zoomControl: false });
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   crossOrigin: ""
 }).addTo(map);
 
-// const points = [];
-// locations.forEach(loc => {
-//   points.push([loc.coordinates[1], loc.coordinates[0]]);
-//   L.marker([loc.coordinates[1], loc.coordinates[0]])
-//     .addTo(map)
-//     .bindPopup(`<h4>Day ${loc.day}: ${loc.description}</h4>`, { autoClose: false })
-//     .openPopup();
-// });
+const points = [];
+locations.forEach(loc => {
+  points.push([loc.coordinates[1], loc.coordinates[0]]);
+  L.marker([loc.coordinates[1], loc.coordinates[0]])
+    .addTo(map)
+    .bindPopup(`<p style={font-size: "20px"}>Day ${loc.day}: ${loc.description}</p>`, { autoClose: false })
+    .openPopup();
+});
 
-// const bounds = L.latLngBounds(points).pad(0.5);
-// map.fitBounds(bounds);
+const bounds = L.latLngBounds(points).pad(0.5);
+map.fitBounds(bounds);
 
-// map.scrollWheelZoom.disable(); //to disable zoom by mouse wheel
+map.scrollWheelZoom.disable(); //to disable zoom by mouse wheel
