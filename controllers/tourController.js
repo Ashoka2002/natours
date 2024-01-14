2e8e130f5d12049bc2cf30b7aa90b7401d35f364
Ashok Choudhary <ashoka8769@gmail.com>
2024-01-14 19:46:55 +0530

    Processing Multiple Images

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -26,9 +26,36 @@ exports.uploadTourImages = upload.fields([
 // upload.single("image"); // for one picture req.file
 // upload.array("image", 5); // for multiple picture req.files
 
-exports.resizeTourImages = (req, res, next) => {
-  console.log("\n", req.files);
-};
+exports.resizeTourImages = catchAsync(async (req, res, next) => {
+  if (!req.files.imageCover && !req.files.images) return next();
+
+  // COVER IMAGE
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333)
+    .toFormat("jpeg")
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`);
+
+  // IMAGES
+  req.body.images = [];
+  await Promise.all(
+    req.files.images.map(async (file, i) => {
+      const fileName = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+
+      await sharp(file.buffer)
+        .resize(2000, 1333)
+        .toFormat("jpeg")
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/tours/${fileName}`);
+
+      req.body.images.push(fileName);
+    })
+  );
+
+  next();
+});
 
 //////////////////
 
