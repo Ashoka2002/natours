a657d76f906c90af77b5f26a21c62622379644c2
Ashok Choudhary <ashoka8769@gmail.com>
2024-01-19 18:49:51 +0530

    ADDING 1 TO TRUST PROXY

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const AppError = require("./utils/appErrors");
 const { webhookCheckout } = require("./controllers/bookingController");
 
 const app = express();
-app.enable("trust proxy", true);
+app.enable("trust proxy", 1);
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
